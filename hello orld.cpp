e682750f9c6015887720e3118c0e7fdb47997e1f
382kts <122964555+382kts@users.noreply.github.com>
2026-04-20 16:00:23 +0530

    add one more line
    a simple cout line

--- hello orld.cpp
+++ hello orld.cpp
@@ -2,6 +2,7 @@
 using namespace stdl;
 int main()
 {
-	cout<<"This is my first programing file one"
+	cout<<"This is my first programing file one";
+	cout<<"This is the 2nd line of program ";
 }
 //codded by priyanka 
